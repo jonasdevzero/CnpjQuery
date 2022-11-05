8a76bf28e5513811aeadd766a6960671177d1342
jonasdevzero <jonasdevzero@gmail.com>
2022-11-05 16:49:52 -0300

    chore: add jest

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  roots: ['<rootDir>/src'],
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  coverageProvider: 'v8',
+  preset: '@shelf/jest-mongodb',
+  transform: {
+    '.+\\.ts$': 'ts-jest',
+  },
+};
