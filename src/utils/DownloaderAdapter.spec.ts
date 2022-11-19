3c8059eb76a49879d94e7250a470994e9136c683
jonasdevzero <jonasdevzero@gmail.com>
2022-11-19 20:05:14 -0300

    test: ensure DownloaderAdapter call http.request with correct param

--- src/utils/DownloaderAdapter.spec.ts
+++ src/utils/DownloaderAdapter.spec.ts
@@ -1,6 +1,12 @@
 import http from 'node:http';
 import { DownloaderAdapter } from './DownloaderAdapter';
 
+jest.mock('node:http', () => ({
+  request() {
+    return null;
+  },
+}));
+
 describe('DownloaderAdapter Util', () => {
   test('Should throw if http.request throws', async () => {
     const sut = new DownloaderAdapter();
@@ -12,5 +18,15 @@ describe('DownloaderAdapter Util', () => {
     });
 
     await expect(sut.download('http://any_url.com')).rejects.toThrow();
+  });
+
+  test('Should call http.request with correct param', async () => {
+    const sut = new DownloaderAdapter();
+
+    const requestSpy = jest.spyOn(http, 'request');
+
+    await sut.download('http://any_url.com');
+
+    expect(requestSpy).toHaveBeenCalledWith('http://any_url.com');
   });
 });
