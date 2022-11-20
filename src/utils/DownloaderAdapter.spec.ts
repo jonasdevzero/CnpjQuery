d90ec00dab9533a95d53ae7eeaef4273b98dd7ee
jonasdevzero <jonasdevzero@gmail.com>
2022-11-19 21:09:23 -0300

    test: ensure DownloaderAdapter handle request response listener

--- src/utils/DownloaderAdapter.spec.ts
+++ src/utils/DownloaderAdapter.spec.ts
@@ -3,11 +3,38 @@ import { DownloaderAdapter } from './DownloaderAdapter';
 
 jest.mock('node:http', () => ({
   request() {
-    return null;
+    return {
+      on: jest.fn((event: string, listener: () => void) => {}),
+    };
   },
 }));
 
 describe('DownloaderAdapter Util', () => {
+  test('Should call http.request with correct param', async () => {
+    const sut = new DownloaderAdapter();
+
+    const requestSpy = jest.spyOn(http, 'request');
+
+    await sut.download('http://any_url.com');
+
+    expect(requestSpy).toHaveBeenCalledWith('http://any_url.com');
+  });
+
+  test('Should handle request response listener', async () => {
+    const sut = new DownloaderAdapter();
+
+    const requestSpy = jest.spyOn(http, 'request');
+
+    await sut.download('http://any_url.com');
+
+    const onSpy = jest.spyOn(requestSpy.mock.results[0].value, 'on');
+
+    const [event, listener] = onSpy.mock.calls[0];
+
+    expect(event).toBe('response');
+    expect(typeof listener).toBe('function');
+  });
+
   test('Should throw if http.request throws', async () => {
     const sut = new DownloaderAdapter();
 
@@ -18,15 +45,5 @@ describe('DownloaderAdapter Util', () => {
     });
 
     await expect(sut.download('http://any_url.com')).rejects.toThrow();
-  });
-
-  test('Should call http.request with correct param', async () => {
-    const sut = new DownloaderAdapter();
-
-    const requestSpy = jest.spyOn(http, 'request');
-
-    await sut.download('http://any_url.com');
-
-    expect(requestSpy).toHaveBeenCalledWith('http://any_url.com');
   });
 });
