9313622bdbf1b6dbc04b1fe593146fb932ef176d
jonasdevzero <jonasdevzero@gmail.com>
2022-11-20 09:42:12 -0300

    feat: ensure DownloaderAdapter throw if an invalid url was given

--- src/utils/DownloaderAdapter.ts
+++ src/utils/DownloaderAdapter.ts
@@ -1,8 +1,15 @@
 import http from 'node:http';
+import { InvalidParamError } from '../presentation/errors/InvalidParamError';
 import { Downloader } from '../presentation/protocols/downloader';
 
 export class DownloaderAdapter implements Downloader {
   async download(url: string): Promise<string> {
+    const isValidUrl = /http[s]?:\/\/.+\.(zip)/g.test(url);
+
+    if (!isValidUrl) {
+      throw new InvalidParamError('url');
+    }
+
     const request = http.request(url);
 
     request.on('response', () => {});
