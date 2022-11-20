d51c9d61db0be2b4b5333a4f7c83a08bfb77e9e4
jonasdevzero <jonasdevzero@gmail.com>
2022-11-19 22:55:03 -0300

    chore: remove coverage when is unit or integration tests

--- jest-integration-config.js
+++ jest-integration-config.js
@@ -1,5 +1,6 @@
 const config = require('./jest.config');
 
 config.testMatch = ['**/*.test.ts'];
+config.collectCoverage = false;
 
 module.exports = config;
--- jest-unit-config.js
+++ jest-unit-config.js
@@ -1,5 +1,6 @@
 const config = require('./jest.config');
 
 config.testMatch = ['**/*.spec.ts'];
+config.collectCoverage = false;
 
 module.exports = config;
