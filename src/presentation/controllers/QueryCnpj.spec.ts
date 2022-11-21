1cf2aa63484762f287af42fddebb86ae48bc0fb7
jonasdevzero <jonasdevzero@gmail.com>
2022-11-21 20:03:41 -0300

    test: ensure QueryCnpjController throw if QueryCnpj throws

--- src/presentation/controllers/QueryCnpj.spec.ts
+++ src/presentation/controllers/QueryCnpj.spec.ts
@@ -1,11 +1,50 @@
+import { QueryCnpj } from '../../domain/useCases/queryCnpj';
 import { QueryCnpjController } from './QueryCnpj';
 
-describe('QueryCnpj Controller', () => {
-  test('Should return 200 if success', () => {
-    const sut = new QueryCnpjController();
+const makeFakeQueryCnpj = (): QueryCnpj => {
+  class QueryCnpjStub implements QueryCnpj {
+    async query(): Promise<void> {
+      // ...
+    }
+  }
 
-    const httpResponse = sut.handle({});
+  return new QueryCnpjStub();
+};
+
+interface SutTypes {
+  sut: QueryCnpjController;
+  queryCnpjStub: QueryCnpj;
+}
+
+const makeSut = (): SutTypes => {
+  const queryCnpjStub = makeFakeQueryCnpj();
+  const sut = new QueryCnpjController(queryCnpjStub);
+
+  return {
+    sut,
+    queryCnpjStub,
+  };
+};
+
+describe('QueryCnpj Controller', () => {
+  test('Should return 200 if success', async () => {
+    const { sut } = makeSut();
+
+    const httpResponse = await sut.handle({});
 
     expect(httpResponse.statusCode).toBe(200);
+  });
+
+  test('Should return 500 if QueryCnpj Model throws', async () => {
+    const { sut, queryCnpjStub } = makeSut();
+
+    jest.spyOn(queryCnpjStub, 'query').mockImplementationOnce(() => {
+      throw new Error();
+    });
+
+    const httpResponse = await sut.handle({});
+
+    expect(httpResponse.statusCode).toBe(500);
+    expect(httpResponse.body).toEqual(new Error());
   });
 });
