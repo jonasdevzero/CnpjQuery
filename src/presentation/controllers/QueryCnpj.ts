078847706485fddd94cc0da8bfd29104ca98143f
jonasdevzero <jonasdevzero@gmail.com>
2022-11-21 20:03:27 -0300

    feat: ensure QueryCnpjController throw if QueryCnpj throws

--- src/presentation/controllers/QueryCnpj.ts
+++ src/presentation/controllers/QueryCnpj.ts
@@ -1,10 +1,26 @@
+import { QueryCnpj } from '../../domain/useCases/queryCnpj';
 import { Controller } from '../protocols/controller';
 import { HttpRequest, HttpResponse } from '../protocols/http';
 
 export class QueryCnpjController implements Controller {
-  handle(request: HttpRequest): HttpResponse {
-    return {
-      statusCode: 200,
-    };
+  private readonly queryCnpj: QueryCnpj;
+
+  constructor(queryCnpj: QueryCnpj) {
+    this.queryCnpj = queryCnpj;
+  }
+
+  async handle(_request: HttpRequest): Promise<HttpResponse> {
+    try {
+      await this.queryCnpj.query();
+
+      return {
+        statusCode: 200,
+      };
+    } catch (error) {
+      return {
+        statusCode: 500,
+        body: error,
+      };
+    }
   }
 }
