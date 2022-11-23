fd6ff8e917edeb53ee8a144555b0a2b2d1534d5c
jonasdevzero <jonasdevzero@gmail.com>
2022-11-23 16:33:31 -0300

    feat: ensure DbQueryCnpj call ZipLoader with correct values

--- src/data/useCases/DbQueryCnpj.ts
+++ src/data/useCases/DbQueryCnpj.ts
@@ -1,14 +1,27 @@
+import { DataUrlModel } from '../../domain/models/DataUrl';
 import { QueryCnpj } from '../../domain/useCases/QueryCnpj';
+import { ZipLoader } from '../../presentation/protocols/ZipLoader';
 import { ListDataUrlRepository } from '../protocols/ListDataUrlRepository';
 
 export class DbQueryCnpj implements QueryCnpj {
   private readonly listDataUrlRepository: ListDataUrlRepository;
+  private readonly zipLoader: ZipLoader;
 
-  constructor(listDataUrlRepository: ListDataUrlRepository) {
+  constructor(
+    listDataUrlRepository: ListDataUrlRepository,
+    zipLoader: ZipLoader,
+  ) {
     this.listDataUrlRepository = listDataUrlRepository;
+    this.zipLoader = zipLoader;
   }
 
   async query(): Promise<void> {
-    await this.listDataUrlRepository.list();
+    const dataUrls = await this.listDataUrlRepository.list();
+
+    await Promise.all(dataUrls.map((dataUrl) => this.loadDataUrl(dataUrl)));
+  }
+
+  private async loadDataUrl(dataUrl: DataUrlModel) {
+    await this.zipLoader.load(dataUrl.url);
   }
 }
--- src/domain/models/DataUrl.ts
+++ src/domain/models/DataUrl.ts
@@ -1,5 +1,12 @@
+export type DataUrlType =
+  | 'COMPANY'
+  | 'ESTABLISHMENT'
+  | 'SIMPLES'
+  | 'PARTNER'
+  | 'GENERAL';
+
 export interface DataUrlModel {
   id: string;
   url: string;
-  type: string;
+  type: DataUrlType;
 }
