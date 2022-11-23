91d95dcf53131a4af20072b52a38f553ceabade7
jonasdevzero <jonasdevzero@gmail.com>
2022-11-23 17:21:43 -0300

    test: ensure DbQueryCnpj call UpsertCompanyRepository with correct values

--- src/data/useCases/DbQueryCnpj.spec.ts
+++ src/data/useCases/DbQueryCnpj.spec.ts
@@ -1,9 +1,11 @@
+import { UpsertCompanyModel } from '../../domain/models/Company';
 import { DataUrlModel } from '../../domain/models/DataUrl';
 import {
   ZipLoader,
   ZipLoaderStream,
 } from '../../presentation/protocols/ZipLoader';
 import { ListDataUrlRepository } from '../protocols/ListDataUrlRepository';
+import { UpsertCompanyRepository } from '../protocols/UpsertCompanyRepository';
 import { DbQueryCnpj } from './DbQueryCnpj';
 
 const makeFakeDataUrls = (): DataUrlModel[] => [
@@ -34,6 +36,16 @@ const makeFakeDataUrls = (): DataUrlModel[] => [
   },
 ];
 
+const makeUpsertCompanyRepository = (): UpsertCompanyRepository => {
+  class UpsertCompanyRepositoryStub implements UpsertCompanyRepository {
+    async upsert(data: UpsertCompanyModel) {
+      // ...
+    }
+  }
+
+  return new UpsertCompanyRepositoryStub();
+};
+
 const makeZipLoader = (): ZipLoader => {
   class ZipLoaderStub implements ZipLoader {
     async load(url: string): Promise<ZipLoaderStream> {
@@ -59,19 +71,26 @@ const makeListDataUrlRepository = (): ListDataUrlRepository => {
 interface SutTypes {
   listDataUrlRepositoryStub: ListDataUrlRepository;
   zipLoaderStub: ZipLoader;
+  upsertCompanyRepositoryStub: UpsertCompanyRepository;
   sut: DbQueryCnpj;
 }
 
 const makeSut = (): SutTypes => {
   const listDataUrlRepositoryStub = makeListDataUrlRepository();
   const zipLoaderStub = makeZipLoader();
+  const upsertCompanyRepositoryStub = makeUpsertCompanyRepository();
 
-  const sut = new DbQueryCnpj(listDataUrlRepositoryStub, zipLoaderStub);
+  const sut = new DbQueryCnpj(
+    listDataUrlRepositoryStub,
+    zipLoaderStub,
+    upsertCompanyRepositoryStub,
+  );
 
   return {
     sut,
     listDataUrlRepositoryStub,
     zipLoaderStub,
+    upsertCompanyRepositoryStub,
   };
 };
 
@@ -118,5 +137,46 @@ describe('DbQueryCnpj UseCase', () => {
     expect(errorEvent).toBe('error');
     expect(typeof dataListener).toBe('function');
     expect(typeof errorListener).toBe('function');
+  });
+
+  test('Should call UpsertCompanyRepository with correct values', async () => {
+    const {
+      sut,
+      listDataUrlRepositoryStub,
+      zipLoaderStub,
+      upsertCompanyRepositoryStub,
+    } = makeSut();
+
+    jest
+      .spyOn(listDataUrlRepositoryStub, 'list')
+      .mockImplementationOnce(async () => {
+        return [{ id: 'any_id', url: 'http://any_url.zip', type: 'COMPANY' }];
+      });
+
+    const zipLoaderSpy = jest.spyOn(zipLoaderStub, 'load');
+    const upsertSpy = jest.spyOn(upsertCompanyRepositoryStub, 'upsert');
+
+    await sut.query();
+
+    const zipLoaderResult = await zipLoaderSpy.mock.results[0].value;
+    const zipLoaderOnSpy = jest.spyOn(zipLoaderResult, 'on');
+
+    const dataListener = jest.fn(
+      zipLoaderOnSpy.mock.calls[0][1] as (data: string) => {},
+    );
+
+    dataListener(
+      '"any_base_cnpj";"any_corporate_name";"any_legal_nature";"any_qualification";"any_capital";"any_size";"any_federative_entity"',
+    );
+
+    expect(upsertSpy).toHaveBeenCalledWith({
+      baseCnpj: 'any_base_cnpj',
+      corporateName: 'any_corporate_name',
+      legalNature: 'any_legal_nature',
+      qualification: 'any_qualification',
+      capital: 'any_capital',
+      size: 'any_size',
+      federativeEntity: 'any_federative_entity',
+    });
   });
 });
