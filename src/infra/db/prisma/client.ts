1eeedc186909c780ea1319edec12945b9520d34a
jonasdevzero <jonasdevzero@gmail.com>
2022-11-24 15:19:39 -0300

    feat: add prevention to multiple prisma client

--- src/infra/db/prisma/client.ts
+++ src/infra/db/prisma/client.ts
@@ -1,4 +1,17 @@
 import { PrismaClient } from '@prisma/client';
 
-const prisma = new PrismaClient();
+// eslint-disable-next-line no-undef
+interface CustomNodeJsGlobal extends Global {
+  prisma: PrismaClient;
+}
+
+// Prevent multiple instances of Prisma Client in development
+declare const global: CustomNodeJsGlobal;
+
+const prisma = global.prisma || new PrismaClient();
+
+if (process.env.NODE_ENV === 'development') {
+  global.prisma = prisma;
+}
+
 export default prisma;
