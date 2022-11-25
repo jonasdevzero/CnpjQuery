e162e903784ecdf029aa8e25b4e357223194f7c3
jonasdevzero <jonasdevzero@gmail.com>
2022-11-24 23:32:03 -0300

    test: ensure UpsertEstablishmentPrismaRepository throw if prisma upsert throws

--- src/infra/db/prisma/repositories/cnpjEstablishment/UpsertEstablishmentPrismaRepository.spec.ts
+++ src/infra/db/prisma/repositories/cnpjEstablishment/UpsertEstablishmentPrismaRepository.spec.ts
@@ -1,40 +1,48 @@
 import { prismaMock } from '../../clientMock';
 import { UpsertEstablishmentPrismaRepository } from './UpsertEstablishmentPrismaRepository';
 
+const makeFakeEstablishmentData = () => {
+  const establishmentData = {
+    baseCnpj: 'any_base_cnpj',
+    cnpj: 'any_cnpj',
+    corporateName: '',
+    cadasterStatus: '',
+    cadasterStatusDate: '',
+    cadasterStatusReason: '',
+    activityStartAt: '',
+    mainCnae: '',
+    secondaryCnae: '',
+    specialStatus: '',
+    specialStatusDate: '',
+    telephone1: '',
+    telephone2: '',
+    fax: '',
+    email: '',
+    address: {
+      cityAbroad: '',
+      countryCode: '',
+      streetDescription: '',
+      street: '',
+      number: '',
+      complement: '',
+      district: '',
+      cep: '',
+      uf: '',
+      city: '',
+    },
+  };
+  return establishmentData;
+};
+
+const makeSut = (): UpsertEstablishmentPrismaRepository => {
+  return new UpsertEstablishmentPrismaRepository();
+};
+
 describe('UpsertEstablishmentPrismaRepository', () => {
   test('Should call prisma upsert with correct values', async () => {
-    const sut = new UpsertEstablishmentPrismaRepository();
+    const sut = makeSut();
 
-    const establishmentData = {
-      baseCnpj: 'any_base_cnpj',
-      cnpj: 'any_cnpj',
-      corporateName: '',
-      cadasterStatus: '',
-      cadasterStatusDate: '',
-      cadasterStatusReason: '',
-      activityStartAt: '',
-      mainCnae: '',
-      secondaryCnae: '',
-      specialStatus: '',
-      specialStatusDate: '',
-      telephone1: '',
-      telephone2: '',
-      fax: '',
-      email: '',
-      address: {
-        cityAbroad: '',
-        countryCode: '',
-        streetDescription: '',
-        street: '',
-        number: '',
-        complement: '',
-        district: '',
-        cep: '',
-        uf: '',
-        city: '',
-      },
-    };
-
+    const establishmentData = makeFakeEstablishmentData();
     const { cnpj, address, ...rest } = establishmentData;
 
     await sut.upsert(establishmentData);
@@ -55,5 +63,15 @@ describe('UpsertEstablishmentPrismaRepository', () => {
         address: { create: establishmentData.address },
       },
     });
+  });
+
+  test('Should throw if prisma upsert throws', async () => {
+    const sut = makeSut();
+
+    prismaMock.cnpjEstablishment.upsert.mockImplementationOnce(() => {
+      throw new Error();
+    });
+
+    await expect(sut.upsert(makeFakeEstablishmentData())).rejects.toThrow();
   });
 });
