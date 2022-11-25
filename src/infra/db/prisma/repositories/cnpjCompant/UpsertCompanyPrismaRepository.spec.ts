f199b9960aba14d0b50ec0ed44d461513b52d5d1
jonasdevzero <jonasdevzero@gmail.com>
2022-11-24 22:06:32 -0300

    test: ensure UpsertCompanyPrismaRepository throw if prisma upsert throws

--- src/infra/db/prisma/repositories/cnpjCompant/UpsertCompanyPrismaRepository.spec.ts
+++ src/infra/db/prisma/repositories/cnpjCompant/UpsertCompanyPrismaRepository.spec.ts
@@ -1,19 +1,28 @@
+import { UpsertCompanyModel } from '../../../../../domain/models/Company';
 import { prismaMock } from '../../clientMock';
 import { UpsertCompanyPrismaRepository } from './UpsertCompanyPrismaRepository';
 
+const makeFakeCompanyData = (): UpsertCompanyModel => {
+  return {
+    baseCnpj: 'any_base_cnpj',
+    corporateName: 'any_corporate_name',
+    legalNature: 'any_legal_nature',
+    qualification: 'any_qualification',
+    capital: '',
+    size: 'any_size',
+    federativeEntity: 'any_federative_entity',
+  };
+};
+
+const makeSut = (): UpsertCompanyPrismaRepository => {
+  return new UpsertCompanyPrismaRepository();
+};
+
 describe('UpsertCompanyPrismaRepository', () => {
   test('Should call prisma upsert with correct values', async () => {
-    const sut = new UpsertCompanyPrismaRepository();
+    const sut = makeSut();
 
-    const companyData = {
-      baseCnpj: 'any_base_cnpj',
-      corporateName: 'any_corporate_name',
-      legalNature: 'any_legal_nature',
-      qualification: 'any_qualification',
-      capital: '',
-      size: 'any_size',
-      federativeEntity: 'any_federative_entity',
-    };
+    const companyData = makeFakeCompanyData();
 
     await sut.upsert(companyData);
 
@@ -22,5 +31,15 @@ describe('UpsertCompanyPrismaRepository', () => {
       create: companyData,
       update: companyData,
     });
+  });
+
+  test('Should throw if prisma upsert throws', async () => {
+    const sut = makeSut();
+
+    prismaMock.cnpjCompany.upsert.mockImplementationOnce(() => {
+      throw new Error('any_error');
+    });
+
+    await expect(sut.upsert(makeFakeCompanyData())).rejects.toThrow(new Error('any_error'));
   });
 });
