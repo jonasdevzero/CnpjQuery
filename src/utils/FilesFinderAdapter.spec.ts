d22b8aa8436996508a75da18593f771229a0c06b
jonasdevzero <jonasdevzero@gmail.com>
2022-11-27 20:33:25 -0300

    test: ensure FilesFinderAdapter return files on success

--- src/utils/FilesFinderAdapter.spec.ts
+++ src/utils/FilesFinderAdapter.spec.ts
@@ -2,12 +2,16 @@ import fs from 'node:fs';
 import { FilesFinderAdapter } from './FilesFinderAdapter';
 
 jest.mock('node:fs', () => ({
-  readdirSync: jest.fn(),
+  readdirSync: jest.fn(() => []),
 }));
+
+const makeSut = (): FilesFinderAdapter => {
+  return new FilesFinderAdapter();
+};
 
 describe('FilesFinderAdapter Util', () => {
   test('Should call fs.readdirSync with correct value', () => {
-    const sut = new FilesFinderAdapter();
+    const sut = makeSut();
 
     const readdirSpy = jest.spyOn(fs, 'readdirSync');
 
@@ -17,5 +21,19 @@ describe('FilesFinderAdapter Util', () => {
 
     expect(readdirSpy).toHaveBeenCalledTimes(1);
     expect(readdirPath.endsWith('any_dir'));
+  });
+
+  test('Should return files on success', () => {
+    const sut = makeSut();
+
+    jest.spyOn(fs, 'readdirSync').mockImplementationOnce(() => {
+      const mockedFiles = ['any_file.routes.ts', 'any_file.ts'];
+      return mockedFiles as any;
+    });
+
+    const files = sut.find('any_dir', '*.routes.ts');
+
+    expect(files.length).toBe(1);
+    expect(files[0].endsWith('any_dir\\any_file.routes.ts')).toBeTruthy();
   });
 });
