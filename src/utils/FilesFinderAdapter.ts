5645b4e8461bde2d7d1728ad100e324517b48f12
jonasdevzero <jonasdevzero@gmail.com>
2022-11-27 20:32:59 -0300

    feat: ensure FilesFinderAdapter return files on success

--- src/utils/FilesFinderAdapter.ts
+++ src/utils/FilesFinderAdapter.ts
@@ -7,14 +7,36 @@ const rootPath = process.cwd();
 export class FilesFinderAdapter implements FilesFinder {
   find(dir: string, match: string | RegExp): string[] {
     const dirPath = this.makeDirPath(dir);
+    const files: string[] = [];
+    const fileMatcher = typeof match === 'string' ? this.makeMatcher(match) : match;
 
-    fs.readdirSync(dirPath);
+    const foundedFiles = fs.readdirSync(dirPath);
 
-    return [];
+    for (const file of foundedFiles) {
+      const fullPath = join(dirPath, file);
+
+      if (fileMatcher.test(file)) files.push(fullPath);
+      if (this.isDirectory(file)) files.push(...this.find(fullPath, fileMatcher));
+    }
+
+    return files;
   }
 
   private makeDirPath(dir: string) {
     const pathFolders = dir.split('/');
     return join(rootPath, ...pathFolders);
+  }
+
+  private makeMatcher(matchString: string): RegExp {
+    const matcher = matchString.replace(/\./g, '\\.').replace(/\*/g, '(.+)');
+    return new RegExp(matcher, 'g');
+  }
+
+  private isDirectory(path: string): boolean {
+    return !this.isFile(path);
+  }
+
+  private isFile(path: string): boolean {
+    return /.+(\..+)$/g.test(path);
   }
 }
