4da0d2bd2438563341bda4396652feead5eb445c
jonasdevzero <jonasdevzero@gmail.com>
2022-11-27 13:06:09 -0300

    test: ensure ListDataUrlPostgresRepository return correct values on success

--- src/infra/db/postgres/repositories/dataUrl/ListDataUrlPostgresRepository.spec.ts
+++ src/infra/db/postgres/repositories/dataUrl/ListDataUrlPostgresRepository.spec.ts
@@ -1,14 +1,32 @@
 import { dbMock } from '../../dbMock';
 import { ListDataUrlPostgresRepository } from './ListDataUrlPostgresRepository';
 
+const makeSut = (): ListDataUrlPostgresRepository => {
+  return new ListDataUrlPostgresRepository();
+};
+
 describe('ListDataUrlPostgresRepository', () => {
   test('Should throw if postgres throws', async () => {
-    const sut = new ListDataUrlPostgresRepository();
+    const sut = makeSut();
 
     dbMock.mockImplementationOnce(() => {
       throw new Error();
     });
 
     await expect(sut.list()).rejects.toThrow();
+  });
+
+  test('Should return correct values on success', async () => {
+    const sut = makeSut();
+
+    const dataUrlList = [{ id: 'any_id', url: 'any_url', type: 'any_type' }];
+
+    dbMock.mockImplementationOnce(() => {
+      return dataUrlList;
+    });
+
+    const result = await sut.list();
+
+    expect(result).toEqual(dataUrlList);
   });
 });
