94f061ba0a268eec9afab9f054f3168480e4d9af
jonasdevzero <jonasdevzero@gmail.com>
2022-12-02 18:53:36 -0300

    feat: ensure UpsertCompanyPostgresRepository update data if company was found

--- src/infra/db/postgres/repositories/cnpjCompany/UpsertCompanyPostgresRepository.ts
+++ src/infra/db/postgres/repositories/cnpjCompany/UpsertCompanyPostgresRepository.ts
@@ -7,12 +7,21 @@ export class UpsertCompanyPostgresRepository implements UpsertCompanyRepository 
     const { baseCnpj, corporateName, legalNature, qualification, capital, size, federativeEntity } =
       data;
 
-    await sql`SELECT 1 FROM "cnpjCompany" WHERE "baseCnpj" = ${baseCnpj};`;
+    const hasCompany = await sql`SELECT 1 FROM "cnpjCompany" WHERE "baseCnpj" = ${baseCnpj};`;
 
-    await sql`
-      INSERT INTO "cnpjCompany"  ("baseCnpj", "corporateName", "legalNature", qualification, capital, size, "federativeEntity")
-        VALUES (${baseCnpj}, ${corporateName}, ${legalNature}, ${qualification}, ${capital}, ${size},
+    const promise = !hasCompany.length
+      ? sql`
+        INSERT INTO "cnpjCompany"  ("baseCnpj", "corporateName", "legalNature", qualification, capital, size, "federativeEntity")
+          VALUES (${baseCnpj}, ${corporateName}, ${legalNature}, ${qualification}, ${capital}, ${size},
             ${federativeEntity || ''});
-    `;
+        `
+      : sql`
+        UPDATE "cnpjCompany"
+        SET "corporateName" = ${corporateName}, "legalNature" = ${legalNature}, "qualification" = ${qualification},
+          capital = ${capital}, size = ${size}, "federativeEntity" = ${federativeEntity || ''}
+        WHERE "baseCnpj" = ${baseCnpj};
+      `;
+
+    await promise;
   }
 }
