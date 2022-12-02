d4d48f243f43b221c8264e3fdb3b8914b750ecae
jonasdevzero <jonasdevzero@gmail.com>
2022-12-02 18:48:02 -0300

    test: ensure UpsertCompanyPostgresRepository insert data if company was not found

--- src/infra/db/postgres/repositories/cnpjCompany/UpsertCompanyPostgresRepository.ts
+++ src/infra/db/postgres/repositories/cnpjCompany/UpsertCompanyPostgresRepository.ts
@@ -4,8 +4,15 @@ import sql from '../../db';
 
 export class UpsertCompanyPostgresRepository implements UpsertCompanyRepository {
   async upsert(data: UpsertCompanyModel): Promise<void> {
-    const { baseCnpj } = data;
+    const { baseCnpj, corporateName, legalNature, qualification, capital, size, federativeEntity } =
+      data;
 
     await sql`SELECT 1 FROM "cnpjCompany" WHERE "baseCnpj" = ${baseCnpj};`;
+
+    await sql`
+      INSERT INTO "cnpjCompany"  ("baseCnpj", "corporateName", "legalNature", qualification, capital, size, "federativeEntity")
+        VALUES (${baseCnpj}, ${corporateName}, ${legalNature}, ${qualification}, ${capital}, ${size},
+            ${federativeEntity || ''});
+    `;
   }
 }
