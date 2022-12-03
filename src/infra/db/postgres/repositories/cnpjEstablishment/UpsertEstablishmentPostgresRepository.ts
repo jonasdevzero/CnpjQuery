75dc6f88088531b517d97015aa87db9787b89bb6
jonasdevzero <jonasdevzero@gmail.com>
2022-12-03 13:11:27 -0300

    feat: ensure UpsertEstablishmentPostgresRepository insert data if establishment was not found

--- src/infra/db/postgres/repositories/cnpjEstablishment/UpsertEstablishmentPostgresRepository.ts
+++ src/infra/db/postgres/repositories/cnpjEstablishment/UpsertEstablishmentPostgresRepository.ts
@@ -4,8 +4,92 @@ import sql from '../../db';
 
 export class UpsertEstablishmentPostgresRepository implements UpsertEstablishmentRepository {
   async upsert(data: UpsertEstablishmentModel): Promise<void> {
-    const { cnpj } = data;
+    const {
+      cnpj,
+      baseCnpj,
+      corporateName,
+      cadasterStatus,
+      cadasterStatusDate,
+      cadasterStatusReason,
+      activityStartAt,
+      mainCnae,
+      secondaryCnae,
+      specialStatus,
+      specialStatusDate,
+      telephone1,
+      telephone2,
+      fax,
+      email,
+      address: {
+        cityAbroad,
+        countryCode,
+        streetDescription,
+        street,
+        number,
+        complement,
+        district,
+        cep,
+        uf,
+        city,
+      },
+    } = data;
 
     await sql`SELECT 1 FROM "cnpjEstablishment" WHERE "cnpj" = ${cnpj};`;
+
+    await sql`
+      INSERT INTO "cnpjEstablishment" (
+          cnpj,
+          "baseCnpj",
+          "corporateName",
+          "cadasterStatus",
+          "cadasterStatusDate",
+          "cadasterStatusReason",
+          "activityStartAt",
+          "mainCnae",
+          "secondaryCnae",
+          "specialStatus",
+          "specialStatusDate",
+          "telephone1",
+          "telephone2",
+          fax,
+          email,
+          "cityAbroad",
+          "countryCode",
+          "streetDescription",
+          "street",
+          "number",
+          "complement",
+          "district",
+          "cep",
+          "uf",
+          "city"
+        ) VALUES (
+          ${cnpj},
+          ${baseCnpj},
+          ${corporateName || ''},
+          ${cadasterStatus},
+          ${cadasterStatusDate},
+          ${cadasterStatusReason},
+          ${activityStartAt},
+          ${mainCnae},
+          ${secondaryCnae || ''},
+          ${specialStatus},
+          ${specialStatusDate},
+          ${telephone1},
+          ${telephone2 || ''},
+          ${fax || ''},
+          ${email},
+          ${cityAbroad},
+          ${countryCode},
+          ${streetDescription},
+          ${street},
+          ${number},
+          ${complement},
+          ${district},
+          ${cep},
+          ${uf},
+          ${city}
+        );
+    `;
   }
 }
