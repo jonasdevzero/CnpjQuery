cbdb25d383728f0610f89bb05a0d1d8ac360e0cc
jonasdevzero <jonasdevzero@gmail.com>
2022-12-14 11:48:00 -0300

    test: ensure ListDataUrlPostgresRepository return correct values on success

--- src/infra/db/postgres/repositories/dataUrl/ListDataUrlPostgresRepository.spec.ts
+++ src/infra/db/postgres/repositories/dataUrl/ListDataUrlPostgresRepository.spec.ts
@@ -19,14 +19,124 @@ describe('ListDataUrlPostgresRepository', () => {
   test('Should return correct values on success', async () => {
     const sut = makeSut();
 
-    const dataUrlList = [{ id: 'any_id', url: 'any_url', type: 'any_type' }];
-
     dbMock.mockImplementationOnce(() => {
-      return dataUrlList;
+      return [
+        {
+          id: 'any_id_4',
+          url: 'http://200.152.38.155/CNPJ/Empresas0.zip',
+          type: 'PARTNER',
+        },
+        {
+          id: 'any_id_2',
+          url: 'http://200.152.38.155/CNPJ/Empresas0.zip',
+          type: 'ESTABLISHMENT',
+        },
+        {
+          id: 'any_id_1',
+          url: 'http://200.152.38.155/CNPJ/Empresas0.zip',
+          type: 'COMPANY',
+        },
+        {
+          id: 'any_id_4',
+          url: 'http://200.152.38.155/CNPJ/Empresas0.zip',
+          type: 'NATURES',
+        },
+        {
+          id: 'any_id_3',
+          url: 'http://200.152.38.155/CNPJ/Empresas0.zip',
+          type: 'SIMPLES',
+        },
+        {
+          id: 'any_id_10',
+          url: 'http://200.152.38.155/CNPJ/Empresas0.zip',
+          type: 'COMPANY',
+        },
+        {
+          id: 'any_id_4',
+          url: 'http://200.152.38.155/CNPJ/Empresas0.zip',
+          type: 'COUNTRIES',
+        },
+        {
+          id: 'any_id_4',
+          url: 'http://200.152.38.155/CNPJ/Empresas0.zip',
+          type: 'CNAE',
+        },
+        {
+          id: 'any_id_4',
+          url: 'http://200.152.38.155/CNPJ/Empresas0.zip',
+          type: 'REASONS',
+        },
+        {
+          id: 'any_id_4',
+          url: 'http://200.152.38.155/CNPJ/Empresas0.zip',
+          type: 'QUALIFICATIONS',
+        },
+        {
+          id: 'any_id_4',
+          url: 'http://200.152.38.155/CNPJ/Empresas0.zip',
+          type: 'CITIES',
+        },
+      ];
     });
 
     const result = await sut.list();
 
-    expect(result).toEqual(dataUrlList);
+    expect(result).toEqual([
+      {
+        id: 'any_id_4',
+        url: 'http://200.152.38.155/CNPJ/Empresas0.zip',
+        type: 'COUNTRIES',
+      },
+      {
+        id: 'any_id_4',
+        url: 'http://200.152.38.155/CNPJ/Empresas0.zip',
+        type: 'CITIES',
+      },
+      {
+        id: 'any_id_4',
+        url: 'http://200.152.38.155/CNPJ/Empresas0.zip',
+        type: 'CNAE',
+      },
+      {
+        id: 'any_id_4',
+        url: 'http://200.152.38.155/CNPJ/Empresas0.zip',
+        type: 'REASONS',
+      },
+      {
+        id: 'any_id_4',
+        url: 'http://200.152.38.155/CNPJ/Empresas0.zip',
+        type: 'NATURES',
+      },
+      {
+        id: 'any_id_4',
+        url: 'http://200.152.38.155/CNPJ/Empresas0.zip',
+        type: 'QUALIFICATIONS',
+      },
+      {
+        id: 'any_id_1',
+        url: 'http://200.152.38.155/CNPJ/Empresas0.zip',
+        type: 'COMPANY',
+      },
+      {
+        id: 'any_id_10',
+        url: 'http://200.152.38.155/CNPJ/Empresas0.zip',
+        type: 'COMPANY',
+      },
+      {
+        id: 'any_id_2',
+        url: 'http://200.152.38.155/CNPJ/Empresas0.zip',
+        type: 'ESTABLISHMENT',
+      },
+      {
+        id: 'any_id_3',
+        url: 'http://200.152.38.155/CNPJ/Empresas0.zip',
+        type: 'SIMPLES',
+      },
+      {
+        id: 'any_id_4',
+        url: 'http://200.152.38.155/CNPJ/Empresas0.zip',
+        type: 'PARTNER',
+      },
+    ]);
   });
 });
