689570990cf6819b80620aaf9cc67b28c2ab0c37
jonasdevzero <jonasdevzero@gmail.com>
2022-12-15 00:15:16 -0300

    test: ensure UpsertLegalNaturePostgresRepository throw if postgres throws

---
+++ src/infra/db/postgres/repositories/legalNature/UpsertLegalNaturePostgresRepository.spec.ts
@@ -0,0 +1,23 @@
+import { dbMock } from '../../dbMock';
+import { UpsertLegalNaturePostgresRepository } from './UpsertLegalNaturePostgresRepository';
+import { UpsertQualificationModel } from '../../../../../domain/models/Qualification';
+
+const makeFakeQualification = (): UpsertQualificationModel => {
+  const qualification = {
+    code: 'any_code',
+    description: 'any_description',
+  };
+  return qualification;
+};
+
+describe('UpsertLegalNaturePostgresRepository', () => {
+  test('Should not throw if postgres throws ', async () => {
+    const sut = new UpsertLegalNaturePostgresRepository();
+
+    dbMock.mockImplementationOnce(() => {
+      throw new Error();
+    });
+
+    await expect(sut.upsert(makeFakeQualification())).rejects.toThrow();
+  });
+});
