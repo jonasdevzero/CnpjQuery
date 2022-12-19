fa49689081105b987ee84486bb0aa7840064c727
jonasdevzero <jonasdevzero@gmail.com>
2022-12-19 18:25:32 -0300

    refactor: move sut creation to a helper method

--- src/data/useCases/DbFindCnpj/DbFindCnpj.spec.ts
+++ src/data/useCases/DbFindCnpj/DbFindCnpj.spec.ts
@@ -2,17 +2,31 @@ import { DbFindCnpj } from './DbFindCnpj';
 import { CnpjModel } from '../../../domain/models/Cnpj';
 import { FindCnpjRepository } from '../../protocols/FindCnpjRepository';
 
+const makeFakeFindCnpjRepository = (): FindCnpjRepository => {
+  class FindCnpjRepositoryStub implements FindCnpjRepository {
+    async find(cnpj: string): Promise<CnpjModel | null> {
+      throw new Error();
+    }
+  }
+
+  return new FindCnpjRepositoryStub();
+};
+
+interface SutTypes {
+  sut: DbFindCnpj;
+  findCnpjRepositoryStub: FindCnpjRepository;
+}
+
+const makeSut = (): SutTypes => {
+  const findCnpjRepositoryStub = makeFakeFindCnpjRepository();
+  const sut = new DbFindCnpj(findCnpjRepositoryStub);
+
+  return { sut, findCnpjRepositoryStub };
+};
+
 describe('DbFindCnpj', () => {
   test('Should throw if FindCnpjRepository throws', async () => {
-    class FindCnpjRepositoryStub implements FindCnpjRepository {
-      async find(cnpj: string): Promise<CnpjModel | null> {
-        throw new Error();
-      }
-    }
-
-    const findCnpjRepositoryStub = new FindCnpjRepositoryStub();
-
-    const sut = new DbFindCnpj(findCnpjRepositoryStub);
+    const { sut } = makeSut();
 
     await expect(sut.find('any_cnpj')).rejects.toThrow();
   });
