f086ebaf5738e45e07989323577bd8c2d80a34b3
jonasdevzero <jonasdevzero@gmail.com>
2022-12-19 16:28:46 -0300

    refactor: move sut creation a helper method

--- src/presentation/controllers/FindCnpj/FindCnpj.spec.ts
+++ src/presentation/controllers/FindCnpj/FindCnpj.spec.ts
@@ -3,16 +3,31 @@ import { FindCnpj } from '../../../domain/useCases/FindCnpj';
 import { serverError } from '../../helpers/httpHelper';
 import { FindCnpjController } from './FindCnpj';
 
+const makeFakeFindCnpj = (): FindCnpj => {
+  class FindCnpjStub implements FindCnpj {
+    async find(cnpj: string): Promise<CnpjModel | null> {
+      throw new Error();
+    }
+  }
+
+  return new FindCnpjStub();
+};
+
+interface SutTypes {
+  sut: FindCnpjController;
+  findCnpjStub: FindCnpj;
+}
+
+const makeSut = (): SutTypes => {
+  const findCnpjStub = makeFakeFindCnpj();
+  const sut = new FindCnpjController(findCnpjStub);
+
+  return { sut, findCnpjStub };
+};
+
 describe('FindCnpj Controller', () => {
   test('Should return 500 if FindCnpj throws', async () => {
-    class FindOneCnpjStub implements FindCnpj {
-      async find(cnpj: string): Promise<CnpjModel | null> {
-        throw new Error();
-      }
-    }
-
-    const findOneCnpjStub = new FindOneCnpjStub();
-    const sut = new FindCnpjController(findOneCnpjStub);
+    const { sut } = makeSut();
 
     const httpResponse = await sut.handle({ params: {} });
 
