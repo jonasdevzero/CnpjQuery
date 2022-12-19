7b2ad8535220ac2e45f833fb2d9eeae0b00cec56
jonasdevzero <jonasdevzero@gmail.com>
2022-12-19 16:37:45 -0300

    test: ensure FindCnpjController return 400 if cnpj was not provided

--- src/presentation/controllers/FindCnpj/FindCnpj.spec.ts
+++ src/presentation/controllers/FindCnpj/FindCnpj.spec.ts
@@ -1,7 +1,8 @@
 import { CnpjModel } from '../../../domain/models/Cnpj';
 import { FindCnpj } from '../../../domain/useCases/FindCnpj';
-import { serverError } from '../../helpers/httpHelper';
+import { badRequest, serverError } from '../../helpers/httpHelper';
 import { FindCnpjController } from './FindCnpj';
+import { MissingParamError } from '../../errors/MissingParamError';
 
 const makeFakeFindCnpj = (): FindCnpj => {
   class FindCnpjStub implements FindCnpj {
@@ -26,10 +27,18 @@ const makeSut = (): SutTypes => {
 };
 
 describe('FindCnpj Controller', () => {
-  test('Should return 500 if FindCnpj throws', async () => {
+  test('Should return 400 if cnpj was not provided', async () => {
     const { sut } = makeSut();
 
     const httpResponse = await sut.handle({ params: {} });
+
+    expect(httpResponse).toEqual(badRequest(new MissingParamError('cnpj')));
+  });
+
+  test('Should return 500 if FindCnpj throws', async () => {
+    const { sut } = makeSut();
+
+    const httpResponse = await sut.handle({ params: { cnpj: 'any_cnpj' } });
 
     expect(httpResponse).toEqual(serverError(new Error()));
   });
