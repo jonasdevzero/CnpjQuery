c52196c029184f296f485f6917cbd39c7692effc
jonasdevzero <jonasdevzero@gmail.com>
2022-12-19 18:05:53 -0300

    test: ensure CnpjValidatorRegexAdapter return true if a valid cnpj was provided

--- src/utils/CnpjValidatorRegexAdapter.spec.ts
+++ src/utils/CnpjValidatorRegexAdapter.spec.ts
@@ -8,4 +8,12 @@ describe('CnpjValidatorRegexAdapter', () => {
 
     expect(isValid).toBe(false);
   });
+
+  test('Should return true if a valid cnpj was provided', async () => {
+    const sut = new CnpjValidatorRegexAdapter();
+
+    const isValid = sut.isValid('00000000383260');
+
+    expect(isValid).toBe(true);
+  });
 });
