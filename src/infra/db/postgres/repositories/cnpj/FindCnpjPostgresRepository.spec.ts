bc9deac477ee1f2f2cb1efc526f7797718e8aee8
jonasdevzero <jonasdevzero@gmail.com>
2022-12-19 20:54:44 -0300

    test: ensure FindCnpjPostgresRepository return null if cnpj was not found

--- src/infra/db/postgres/repositories/cnpj/FindCnpjPostgresRepository.spec.ts
+++ src/infra/db/postgres/repositories/cnpj/FindCnpjPostgresRepository.spec.ts
@@ -1,14 +1,28 @@
 import { dbMock } from '../../dbMock';
 import { FindCnpjPostgresRepository } from './FindCnpjPostgresRepository';
 
+const makeSut = () => {
+  return new FindCnpjPostgresRepository();
+};
+
 describe('FindCnpjPostgresRepository', () => {
   test('Should throw if postgres throws', async () => {
-    const sut = new FindCnpjPostgresRepository();
+    const sut = makeSut();
 
     dbMock.mockImplementationOnce(() => {
       throw new Error();
     });
 
     await expect(sut.find('any_cnpj')).rejects.toThrow();
+  });
+
+  test('Should return null if cnpj was not found', async () => {
+    const sut = makeSut();
+
+    dbMock.mockImplementationOnce(() => []);
+
+    const cnpj = await sut.find('any_cnpj');
+
+    expect(cnpj).toBeNull();
   });
 });
