abd57df10bdb88b446a208c2596ff72dc75a01b0
jonasdevzero <jonasdevzero@gmail.com>
2023-02-05 16:22:23 -0300

    chore: remove unnecessary packages

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -1,14 +1,12 @@
-import 'dotenv/config';
 import '@container/register';
+import 'dotenv/config';
 import fastify from 'fastify';
 import setupMiddlewares from './middlewares';
 import setupRoutes from './routes';
-import setupCache from './cache';
 
 const app = fastify();
 
 setupMiddlewares(app);
 setupRoutes(app);
-setupCache(app);
 
 export default app;
